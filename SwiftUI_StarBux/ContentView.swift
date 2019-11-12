54d3f3966f522b7bf1c9f8f79ae700227a36ab86
bigmsg <travelcar@naver.com>
2019-11-12 14:53:31 +0900

    Initial Commit

---
+++ SwiftUI_StarBux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_StarBux
+//
+//  Created by 양팀장(iMac) on 2019/11/12.
+//  Copyright © 2019 양팀장(iMac). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
